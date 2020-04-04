5fcecfafba7c556de29629628e7d8eeea91c4435
SergeyMotrokhov <leshch2016@yandex.ru>
2020-04-05 01:26:52 +0300

    v1

---
+++ src/test/java/ru/yandex/WebDriverSettings.java
@@ -0,0 +1,39 @@
+package ru.yandex;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.*;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+public class WebDriverSettings {
+    WebDriver chromeDriver;
+
+    @BeforeEach
+    public void setUpYandexTest(){
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver",System.getenv("CHROME_DRIVER"));
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-size=1920,1200");
+        chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+        chromeDriver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+        chromeDriver.manage().timeouts().setScriptTimeout(40, TimeUnit.SECONDS);
+        chromeDriver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+        chromeDriver.quit();
+    }
+
+}
